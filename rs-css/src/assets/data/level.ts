5ed45e2ce7de8c334eafbb61e5b2ad8563381ecf
Gleb Elenev <g.elenev@gmail.com>
2023-06-23 20:38:53 +0300

    feat: add levels and tune styles for them

--- rs-css/src/assets/data/level.ts
+++ rs-css/src/assets/data/level.ts
@@ -26,7 +26,27 @@ export const tasks: Task[] = [
       },
     ],
     answer: 'plate',
-    taskStr: 'Select plates',
+    taskStr: 'Select the plates',
+  },
+
+  {
+    task: [
+      {
+        element: 'steak',
+        tooltip: '<steak>',
+      },
+      {
+        element: 'plate',
+        tooltip: '<plate>',
+      },
+      {
+        element: 'pan',
+        tooltip: '<pan>',
+        isTarget: true,
+      },
+    ],
+    answer: 'pan',
+    taskStr: 'Select the pan',
   },
 
   {
@@ -41,6 +61,10 @@ export const tasks: Task[] = [
         tooltip: '<pan id="red">',
         isTarget: true,
       },
+      {
+        element: 'pan',
+        tooltip: '<pan>',
+      },
     ],
     answer: '#red',
     taskStr: 'Select the plate with red border',
@@ -51,12 +75,6 @@ export const tasks: Task[] = [
       {
         element: 'plate',
         tooltip: '<plate>',
-        childes: [
-          {
-            element: 'steak',
-            tooltip: '<steak>',
-          },
-        ],
       },
       {
         element: 'pan',
@@ -67,6 +85,34 @@ export const tasks: Task[] = [
             tooltip: '<steak>',
             isTarget: true,
           },
+        ],
+      },
+      {
+        element: 'steak',
+        tooltip: '<steak>',
+      },
+    ],
+    answer: 'pan steak',
+    taskStr: 'Select the steak on the pan',
+  },
+
+  {
+    task: [
+      {
+        element: 'pan',
+        tooltip: '<pan>',
+        childes: [
+          {
+            element: 'steak',
+            tooltip: '<steak>',
+          },
+        ],
+      },
+      {
+        element: 'pan',
+        attributes: { id: 'green' },
+        tooltip: '<pan id="green">',
+        childes: [
           {
             element: 'steak',
             tooltip: '<steak>',
@@ -74,9 +120,20 @@ export const tasks: Task[] = [
           },
         ],
       },
+      {
+        element: 'plate',
+        tooltip: '<plate>',
+        childes: [
+          {
+            element: 'steak',
+            tooltip: '<steak id="cooked">',
+            attributes: { id: 'cooked' },
+          },
+        ],
+      },
     ],
-    answer: 'pan steak',
-    taskStr: 'Select steak on the pan and very very very very very long task',
+    answer: '#green steak',
+    taskStr: 'Select the steak on the pan with green border',
   },
 
   {
@@ -87,8 +144,8 @@ export const tasks: Task[] = [
         childes: [
           {
             element: 'steak',
-            attributes: { id: 'cooked' },
-            tooltip: '<steak id="cooked">',
+            attributes: { id: 'cooked', class: 'small' },
+            tooltip: '<steak id="cooked" class="small">',
             isTarget: true,
           },
         ],
@@ -99,7 +156,62 @@ export const tasks: Task[] = [
         childes: [
           {
             element: 'steak',
+            attributes: { class: 'small' },
+            tooltip: '<steak class="small">',
+            isTarget: true,
+          },
+          {
+            element: 'steak',
             tooltip: '<steak>',
+          },
+        ],
+      },
+      {
+        element: 'steak',
+        tooltip: '<steak class="small">',
+        attributes: { class: 'small' },
+        isTarget: true,
+      },
+    ],
+    answer: '.small',
+    taskStr: 'Select the small steaks',
+  },
+
+  {
+    task: [
+      {
+        element: 'pan',
+        tooltip: '<pan id="green">',
+        attributes: { id: 'green' },
+        childes: [
+          {
+            element: 'steak',
+            attributes: { class: 'small' },
+            tooltip: '<steak class="small">',
+            isTarget: true,
+          },
+        ],
+      },
+      {
+        element: 'plate',
+        tooltip: '<plate>',
+        childes: [
+          {
+            element: 'steak',
+            attributes: { id: 'cooked', class: 'small' },
+            tooltip: '<steak id="cooked" class="small">',
+          },
+        ],
+      },
+      {
+        element: 'pan',
+        tooltip: '<pan>',
+        childes: [
+          {
+            element: 'steak',
+            attributes: { class: 'small' },
+            tooltip: '<steak class="small">',
+            isTarget: true,
           },
           {
             element: 'steak',
@@ -108,8 +220,144 @@ export const tasks: Task[] = [
         ],
       },
     ],
-    answer: '#cooked',
-    taskStr: 'Select cooked steak',
+    answer: 'pan .small',
+    taskStr: 'Select the small steaks on the pans',
+  },
+
+  {
+    task: [
+      {
+        element: 'steak',
+        tooltip: '<steak>',
+      },
+      {
+        element: 'plate',
+        tooltip: '<plate>',
+        isTarget: true,
+      },
+      {
+        element: 'pan',
+        tooltip: '<pan>',
+        isTarget: true,
+        childes: [
+          {
+            element: 'steak',
+            tooltip: '<steak>',
+          },
+        ],
+      },
+      {
+        element: 'pan',
+        tooltip: '<pan id="green">',
+        attributes: { id: 'green' },
+        isTarget: true,
+        childes: [
+          {
+            element: 'steak',
+            attributes: { class: 'small' },
+            tooltip: '<steak class="small">',
+          },
+        ],
+      },
+      {
+        element: 'steak',
+        tooltip: '<steak>',
+      },
+    ],
+    answer: 'pan, plate',
+    taskStr: 'Select all pans and plates',
+  },
+
+  {
+    task: [
+      {
+        element: 'pan',
+        tooltip: '<pan>',
+        isTarget: true,
+        childes: [
+          {
+            element: 'steak',
+            tooltip: '<steak>',
+            isTarget: true,
+          },
+          {
+            element: 'steak',
+            attributes: { class: 'small' },
+            tooltip: '<steak class="small">',
+            isTarget: true,
+          },
+        ],
+      },
+      {
+        element: 'pan',
+        tooltip: '<pan id="green">',
+        attributes: { id: 'green' },
+        isTarget: true,
+        childes: [
+          {
+            element: 'steak',
+            attributes: { class: 'small' },
+            tooltip: '<steak class="small">',
+            isTarget: true,
+          },
+        ],
+      },
+      {
+        element: 'fish',
+        tooltip: '<fish>',
+        isTarget: true,
+      },
+    ],
+    answer: '*',
+    taskStr: 'Select all',
+  },
+
+  {
+    task: [
+      {
+        element: 'pan',
+        tooltip: '<pan>',
+        childes: [
+          {
+            element: 'steak',
+            tooltip: '<steak>',
+            isTarget: true,
+          },
+          {
+            element: 'fish',
+            attributes: { class: 'small' },
+            tooltip: '<fish class="small">',
+            isTarget: true,
+          },
+        ],
+      },
+      {
+        element: 'pan',
+        tooltip: '<pan id="green">',
+        attributes: { id: 'green' },
+        childes: [
+          {
+            element: 'steak',
+            attributes: { class: 'small' },
+            tooltip: '<steak class="small">',
+            isTarget: true,
+          },
+        ],
+      },
+      {
+        element: 'plate',
+        tooltip: '<plate>',
+        childes: [
+          {
+            element: 'steak',
+            attributes: { class: 'small' },
+            tooltip: '<steak class="small">',
+          },
+        ],
+      },
+    ],
+    answer: 'pan *',
+    taskStr: 'Select all on the pans',
   },
 
 ];
