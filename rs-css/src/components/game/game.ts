9940524f5e938b9f10ea5e72706f05fe6fbfe37f
Gleb Elenev <g.elenev@gmail.com>
2023-06-26 19:20:38 +0300

    refactor: use model to handle operations with task

--- rs-css/src/components/game/game.ts
+++ rs-css/src/components/game/game.ts
@@ -1,3 +1,4 @@
+import { TaskModel } from '../../model/taskModel';
 import Table from '../table/table';
 import HTMLViewer from '../editor-display/html-viewer';
 import ElementBuilder from '../../common/elementBuilder';
@@ -10,21 +11,21 @@ import { Status, SavedStatus } from '../../utils/types';
 type JsonStatus = [number, Status];
 
 export default class Game {
-  public menu: Menu;
+  public menu: Menu = new Menu(tasks);
 
   public header: ElementBuilder;
 
-  public table: Table;
+  public table: Table = new Table();
 
   public input: EditorInput;
 
-  public viewer: HTMLViewer;
-
-  public levelCounter: number;
+  public viewer: HTMLViewer = new HTMLViewer();
 
   private status: Status = 'uncompleted';
 
   private statuses: SavedStatus = new Map();
+
+  private model: TaskModel = new TaskModel();
 
   constructor(parent: HTMLElement) {
     const section = new ElementBuilder({ tag: 'section', classNames: ['game'] });
@@ -32,11 +33,9 @@ export default class Game {
       tag: 'h1',
       classNames: ['game-task'],
     });
-    this.levelCounter = localStorage.getItem('currLvl') ? Number(localStorage.getItem('currLvl')) : 0;
-    this.menu = new Menu(tasks);
-    this.table = new Table();
+    const levelCounter = localStorage.getItem('currLvl') ? Number(localStorage.getItem('currLvl')) : 0;
+    this.model.setCurrLvl(levelCounter);
     this.input = new EditorInput(parent);
-    this.viewer = new HTMLViewer();
     section.addInner(this.header);
     parent.append(this.menu.createElement());
     section.addInner(this.table);
@@ -63,16 +62,15 @@ export default class Game {
     this.setTask();
     this.input.submit.addClick(() => { this.validateInput(this.input.getValue()); });
     document.addEventListener('keypress', (e: KeyboardEvent) => {
-      this.input.input.el.focus();
+      this.input.editor.focus();
       if (e.key === 'Enter') {
         e.preventDefault();
         this.validateInput(this.input.getValue());
       }
     });
-    document.addEventListener('click', () => this.input.editor.focus());
     this.menu.levels.forEach((element, index) => {
       element.el.addEventListener('click', () => {
-        this.levelCounter = index;
+        this.model.setCurrLvl(index);
         this.status = 'uncompleted';
         this.setTask();
       });
@@ -94,13 +92,14 @@ export default class Game {
 
   private setTask(): void {
     this.save();
+    const task = this.model.getCurrentTask();
     this.input.submit.el.removeAttribute('disabled');
     this.input.help.el.removeAttribute('disabled');
-    this.header.addText(tasks[this.levelCounter].taskStr);
-    this.menu.setActive(this.levelCounter);
+    this.header.addText(task.taskStr);
+    this.menu.setActive(this.model.getCurrLvl());
     this.input.setValue('');
-    this.table.addTask(tasks[this.levelCounter]);
-    this.viewer.addTask(tasks[this.levelCounter]);
+    this.table.addTask(task);
+    this.viewer.addTask(task);
     const itemsLength = this.table.plates.length;
     for (let i = 0; i < itemsLength; i += 1) {
       const plates = this.table.plates[i];
@@ -113,7 +112,7 @@ export default class Game {
   }
 
   private save(): void {
-    localStorage.setItem('currLvl', `${this.levelCounter}`);
+    localStorage.setItem('currLvl', `${this.model.getCurrLvl()}`);
     localStorage.setItem('save', JSON.stringify([...this.statuses]));
   }
 
@@ -123,10 +122,10 @@ export default class Game {
   }
 
   private loadNext(): void {
-    const index = this.levelCounter;
+    const index = this.model.getCurrLvl();
     this.statuses.set(index, this.status);
     if (index < tasks.length - 1) {
-      this.levelCounter += 1;
+      this.model.upLvl();
       setTimeout(() => {
         this.setTask();
         this.menu.setStatus(this.status, index);
@@ -146,23 +145,23 @@ export default class Game {
   }
 
   private checkInput(selector: string): void {
-    const elements = this.table.el.querySelectorAll(`${selector}`);
+    const userElements = this.table.el.querySelectorAll(`${selector}`);
+    const correctElements = this.table.el.querySelectorAll(this.model.getAnswer());
     let win = true;
-    if (elements.length === 0) {
+    if (userElements.length === 0) {
       win = false;
       this.shake();
     }
-    elements.forEach((el) => {
-      const returnedEL = this.table.map.get(el);
-      if (!returnedEL?.isTarget || elements.length !== this.table.targets.length) win = false;
+    userElements.forEach((userEl, index) => {
+      if (userEl !== correctElements[index]) win = false;
     });
     if (!win) {
-      elements.forEach((el) => {
+      userElements.forEach((el) => {
         const returnedEL = this.table.map.get(el);
         returnedEL?.playWrong();
       });
     } else if (win) {
-      elements.forEach((el) => {
+      userElements.forEach((el) => {
         const returnedEL = this.table.map.get(el);
         returnedEL?.playWin();
       });
@@ -182,7 +181,7 @@ export default class Game {
   private getHelp(): void {
     this.input.help.addClick(() => {
       this.status = 'help';
-      this.input.printValue(tasks[this.levelCounter].answer);
+      this.input.printValue(this.model.getAnswer());
     });
   }
 
@@ -190,7 +189,7 @@ export default class Game {
     localStorage.removeItem('save');
     localStorage.removeItem('currLvl');
     this.statuses.clear();
-    this.levelCounter = 0;
+    this.model.setCurrLvl(0);
     this.status = 'uncompleted';
     this.menu.resetStatuses();
     this.setTask();
